d43e7564c0efc54ffc0974ec409d261cff42d3d3
Yassine Zeriouh <yassine@zeriouh.io>
2021-01-12 18:29:39 +0100

    Automatically create default placeholder if none is found

--- src/lazy-view.js
+++ src/lazy-view.js
@@ -1,7 +1,9 @@
 window.onload = function () {
   console.log('dom ready');
 
-  document.head.insertAdjacentHTML("beforeend", `<style>[lazy-placeholder]{display:none !important;}</style>`)
+  document.head.insertAdjacentHTML("beforeend", `<style>[lazy-placeholder]{display:none !important;}</style>`);
+
+  let placeholder;
 
   const request = (url, type = 'GET', body = '') => {
     return new Promise((resolve, reject) => {
@@ -75,14 +77,29 @@ window.onload = function () {
         });
 
         // node.removeChild()
-        
+
         // console.log('node children', node);
       }
     }
   };
 
   const copyPlaceholder = (element) => {
-    const placeholder = document.querySelector('[lazy-placeholder]');
+    if (!!document.querySelector('[lazy-placeholder]')) {
+    } else {
+      document.body.insertAdjacentHTML('beforeend', `<style>@keyframes spin {to {transform: rotate(360deg);}}.animate-spin {animation: spin 1s linear infinite;}</style>
+      <div style="position:absolute;top:0;right:0;bottom:0;left:0;z-index:10;background: #f4f4f5" lazy-placeholder>
+      <div style="width: 100%; height: 100%;display: flex; align-items: center; justify-content: center;">
+        <svg class="animate-spin" style="color:#3f3f46;width:2rem;height:2rem" xmlns="http://www.w3.org/2000/svg" fill="none"
+             viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+          </path>
+        </svg>
+      </div>
+    </div>`);
+    }
+    placeholder = document.querySelector('[lazy-placeholder]');
     const clone = placeholder.cloneNode(true);
     const newElement = element.appendChild(clone);
     newElement.removeAttribute('lazy-placeholder');
